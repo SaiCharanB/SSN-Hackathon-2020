076d98fba2f497fc742214db1ddefab472fa17fb
SaiCharanB <62072196+SaiCharanB@users.noreply.github.com>
2020-03-13 23:42:41 +0530

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,16 +1,23 @@
 from twilio.rest import Client
+import subprocess
+import os
 
-account_sid = 'enter account sid'
-auth_token = 'enter auth token'
+
+proc=subprocess.Popen('imgur-uploader pic.jpg',shell=True,stdout=subprocess.PIPE,)
+output=proc.communicate()
+string=str(output)
+final=string[string.find('https://'):string.find('n\',')]
+final=final[0:len(final)-1]
+
+account_sid = 'AC524937e65452ba5e47668ba69c969a9d'
+auth_token = '589cb8848460da3cc21c7b2a0a0ae9d3'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Alert. Intruder!",
-                     from_='ENTER SENDER NO',
-		     media_url=[ENTER MEDIA LINK],
-                     to='ENTER RECEIVER NO'
+                     from_='+15153934072',
+		     media_url=final ,
+                     to='+919940684356'
                  )
-
-print(message.sid)
 
